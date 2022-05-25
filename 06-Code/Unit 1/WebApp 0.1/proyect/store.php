402d5c64652cae89cfb8637b2aa7c02d4e4904ba
Bryan Sebastián Yanzapanta Ontaneda <bsyanzapanta@espe.edu.ec>
2022-05-25 00:23:48 -0500

    store

---
+++ 06-Code/Unit 1/WebApp 0.1/proyect/store.php
@@ -0,0 +1,127 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+<title>Store</title>
+</head>
+<body >
+<header>
+            <div class="nav-cont flex-box sticky">
+                <h2 class="nav-title"> BLIZZ CMS </h2>
+                <nav class=" navbar flex-box justify-left">
+                    <div><a href="/index.php">Start</a></div>
+                    <div><a href="">News</a></div>
+                    <div><a href="">Store</a></div>
+                    <div><a href="">About</a></div>
+                </nav>
+            </div>
+    </header>  
+    <section class="intro3">
+       <h1 class="store">store</h1>
+       <p>Top Items</p>
+ <table class="table table-dark">
+  <thead>
+    ...
+  </thead>
+  <tbody>
+    <tr class="table-active">
+      ...
+    </tr>
+    <tr>
+      ...
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2" class="table-active">Larry the Bird</td>
+      <td><img src="img/store1.jpg"></td>
+    </tr>
+  </tbody>
+  <thead>
+    ...
+  </thead>
+  <tbody>
+    <tr class="table-active">
+      ...
+    </tr>
+    <tr>
+      ...
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2" class="table-active">Larry the Bird</td>
+      <td><img src="img/store2.jpg"></td>
+    </tr>
+  </tbody>
+  <thead>
+    ...
+  </thead>
+  <tbody>
+    <tr class="table-active">
+      ...
+    </tr>
+    <tr>
+      ...
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2" class="table-active">Larry the Bird</td>
+      <td><img src="img/store3.jpg"></td>
+    </tr>
+  </tbody>
+  <thead>
+    ...
+  </thead>
+  <tbody>
+    <tr class="table-active">
+      ...
+    </tr>
+    <tr>
+      ...
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2" class="table-active">Larry the Bird</td>
+      <td><img src="img/store4.jpg"></td>
+    </tr>
+  </tbody>
+  <thead>
+      <br>
+    ...
+  </thead>
+  <tbody>
+    <tr class="table-active">
+      ...
+    </tr>
+    <tr>
+      ...
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2" class="table-active">Larry the Bird</td>
+      <td><img src="img/store1.jpg"></td>
+    </tr>
+  </tbody>
+</table>
+    </section>
+    
+    <footer class="foot">
+        <div class="container">
+            To get more information <br>
+            contact us in the next social networks.
+            <div class="flex-box social-network">
+                <div class=""> Facebook</div>
+                <div class=""> Twitter</div>
+                <div class=""> Github </div>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
