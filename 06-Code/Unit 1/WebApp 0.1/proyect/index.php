40be2f26cd13a2e81f065ec7ef5e6c8cb04b2eef
Christopher <yepecri123@gmail.com>
2022-05-24 22:38:30 -0500

    Intro page

--- 06-Code/Unit 1/WebApp 0.1/proyect/index.php
+++ 06-Code/Unit 1/WebApp 0.1/proyect/index.php
@@ -4,12 +4,87 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
-    <div> a </div>
-    <?php
-        echo "My first PHP script!";
-        ?>
+    <header>
+            <div class="nav-cont flex-box sticky">
+                <h2 class="nav-title"> BLIZZ CMS </h2>
+                <nav class=" navbar flex-box justify-left">
+                    <div><a href="">Start</a></div>
+                    <div><a href="">News</a></div>
+                    <div><a href="">Store</a></div>
+                    <div><a href="">About</a></div>
+                </nav>
+            </div>
+    </header>
+    <section class="intro">
+        <h2 class="intro-title"> BLIZZ CMS</h2>
+        <div class="container centered">
+            <br>
+            <br>
+            <br>
+            <br>
+            <p >
+            where the world of new content begins 
+            <p>
+            <br>
+            <br>
+            <br>
+            <p >
+                A new world is wainting you to conquer and enjoy
+                </p>
+            <br>
+            <br>
+        </div>
+    </section>
+    <section class="container">
+        <div>
+            <h2> What does this server offer?</h2>
+            <br>
+            <br>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error ipsam repellat eligendi quidem, officiis culpa eos doloremque ullam beatae saepe tempora similique in eaque veritatis reprehenderit, dicta sit est facilis?
+            </p>
+            <br>
+            <br>
+        </div>
+    </section>
+    <section class="dark-blue">
+        <div class="container">
+            <h2>Why you should choose us?</h2>
+            <br>
+            <br>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius porro ab, magnam dolorem minus est aperiam a. Praesentium temporibus ea, dolor itaque alias cupiditate iure, voluptatum voluptas modi fuga ipsa!
+            </p>
+            <br>
+            <br>
+        </div>
+    </section>
+    <section class="intro-2">
+        <div class="container">
+            <h2>The best posibilties with the less price</h2>
+            <br>
+            <br>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius porro ab, magnam dolorem minus est aperiam a. Praesentium temporibus ea, dolor itaque alias cupiditate iure, voluptatum voluptas modi fuga ipsa!
+            </p>
+            <br>
+            <br>
+        </div>
+    </section>
+    <footer class="foot">
+        <div class="container">
+            To get more information <br>
+            contact us in the next social networks.
+            <div class="flex-box social-network">
+                <div class=""> Facebook</div>
+                <div class=""> Twitter</div>
+                <div class=""> Github </div>
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
