44c0a48a228dc7d1aba10967791fa2b2ec8e12fc
Sebastian <sazapata2@espe.edu.ec>
2022-05-30 08:01:35 -0500

    Web End

---
+++ 06-Code/html/template/icecrown/tpl/footer.php
@@ -0,0 +1,15 @@
+<?php
+
+use SebastianBergmann\Timer\Timer;
+
+?>
+<div class="row">
+    <div class="text-center box1">
+        Developed by <a href="https://discord.gg/jdwGVjbRnt">MoonShard-WoW</a>
+        - <?php echo "Load " . Timer::resourceUsage(); ?>
+    </div>
+</div>
+</div>
+</div>
+</body>
+</html>
---
+++ 06-Code/html/template/icecrown/tpl/howtoconnect.php
@@ -0,0 +1,11 @@
+<?php
+
+?>
+<div class="content_box1" style="line-height: 1.5;">
+    <p>1. <?php elang('create_account_tip1'); ?></p>
+    <p>2. <?php elang('create_account_tip2'); ?></p>
+    <p>3. <?php elang('create_account_tip3'); ?></p>
+    <p>4. <?php elang('create_account_tip4'); ?></p>
+    <p style="text-align: center;font-weight: bold;color:darkred">set
+        realmlist <?php echo get_config('realmlist'); ?></p>
+</div>
