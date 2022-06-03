df421f53429cb424665afd9cb04c8b0c1ece13a4
unknown <sebas_alexander_@hotmail.com>
2022-06-03 01:29:28 -0500

    Fix Discord Api

---
+++ 06-Code/Unit 1/Web 1.3/html/template/icecrown/tpl/footer.php
@@ -0,0 +1,15 @@
+<?php
+
+use SebastianBergmann\Timer\Timer;
+
+?>
+<div class="row">
+    <div class="text-center box1">
+        Desarrollado por <a href="https://discord.gg/jdwGVjbRnt">MoonShard-WoW</a>
+        - <?php echo "Carga " . Timer::resourceUsage(); ?>
+    </div>
+</div>
+</div>
+</div>
+</body>
+</html>
