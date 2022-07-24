107136609ff2422e46c2ac7bf33348620ee98402
Bryan Sebastián Yanzapanta Ontaneda <bsyanzapanta@espe.edu.ec>
2022-07-24 15:06:15 -0500

    discord api

---
+++ 06-Code/Unit 2/Additional features/discord.js
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import '../pages/stylesComp.css'
+
+const Discord = () => (
+  <div className='discord-style'>
+    <div className='discord-text'>
+   <iframe title="discord" src="https://discord.com/widget?id=1000835556403580929&theme=dark" width="450" height="270" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+  </div>
+  </div>
+
+); export default Discord
