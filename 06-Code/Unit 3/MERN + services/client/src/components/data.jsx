3f8f22463b9d594faa258d6c0e5598a286e539e1
Jonathan Ismael Zapata Sandoval <84453014+jizapata3@users.noreply.github.com>
2022-08-28 19:52:55 -0500

    Store prototype

---
+++ 06-Code/Unit 3/MERN + services/client/src/components/data.jsx
@@ -0,0 +1,30 @@
+import img1 from './data/3.png'
+import img2 from './data/4.jpg'
+import img3 from './data/5.jpg'
+const data ={
+    productData:[
+        {
+            id:1,
+            img: img1,
+            title:'BaronGold',
+            price:5.55,
+
+        },
+        {
+            id:2,
+            img: img2,
+            title:'WeaknessKOL',
+            price:6.99,
+
+        },
+        {
+            id:3,
+            img: img3,
+            title:'GalioSniz',
+            price:9.99,
+
+        },
+        
+    ]
+};
+export default data;
--- 06-Code/Unit 3/MERN + services/client/src/components/onProcess.jsx
+++ 06-Code/Unit 3/MERN + services/client/src/components/onProcess.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import '../assets/css/components/onProcess.css';
-
+import {Link} from 'react-router-dom'
+import Platos from './weapons';
+import data from './data';
+import { CartProvider } from 'react-use-cart';
 const onProcess = () => {
-    return (
-        <div className='not-available'>
-            <h2>
-                THIS PAGE IS NOT AVAILABLE AT THE MOMENT
-
-            </h2>
-        </div>
-    );
+    return ( <>
+          <section className='text-center  mt-3 py-4 container'>   
+          <div className='row justify-content-center'>
+            {data.productData.map((item,index)=>
+            {
+              return(<Platos 
+                img={item.img}  
+                title={item.title}
+                 price={item.price} 
+                 key={index} 
+                 item={item}/>)
+            })} 
+            </div>  
+  </section> 
+    </>
+   )
 };
+
 
 export default onProcess;
